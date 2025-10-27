1b472519ee7dbfcc68653c0a082a015cb9fc1492
earante <arante.emmanuel2017@gmail.com>
2025-10-28 07:53:03 +0800

    feat: Generat HTML for cart items

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import {useEffect, useState} from "react";
 import axios from "axios";
 import { Header } from "../components/Header.jsx";
+import { formatMoney } from "../utils/money.js";
 import "./HomePage.css";
 
 export function HomePage({cart}) {
@@ -45,7 +46,7 @@ export function HomePage({cart}) {
                   <div className="product-rating-count link-primary">{product.rating.count}</div>
                 </div>
 
-                <div className="product-price">${(product.priceCents / 100).toFixed(2)}</div>
+                <div className="product-price">{formatMoney(product.priceCents)}</div>
 
                 <div className="product-quantity-container">
                   <select>
